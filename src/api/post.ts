89fca85ba26f8bdf89e8936f1878311dd31a5464
Yasunari_Kainuna <yasu6491@icloud.com>
2021-06-03 16:27:12 +0900

    add:投稿一覧画面追加

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,10 +1,19 @@
 import Client from '@/api/client'
-// @ts-ignore
-import { Post, PostForRequest } from '@/types/post'
+import { Post, PostForRequest } from '@/types//post'
 import {
   getAuthDataFromStorage
 } from '@/utils/auth-data'
 import { AxiosResponse } from 'axios'
+
+export const getPosts = async () => {
+  return await Client.get('/posts', { headers: getAuthDataFromStorage() })
+    .then((res: AxiosResponse<Post[]>) => {
+      return res.data
+    })
+    .catch((err) => {
+      return err.response
+    })
+}
 
 export const createPost = async (formData: PostForRequest) => {
   return await Client.post(
